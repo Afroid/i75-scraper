b2ff98956cc833b78e1fe7ab35ee574d6d10f148
The John <jsqueet08@gmail.com>
2025-06-25 20:41:55 -0400

    chore: Updated a couple scripts for Jest, added a runtime dev dependency because I switched the Lambda fn to use NodejsFunction in the scraper stack, assigned an account and region, updated the time format in mlbFetcher because of UTC discrepancies.

--- infra/lib/i75-scraper-stack.ts
+++ infra/lib/i75-scraper-stack.ts
@@ -1,33 +1,42 @@
+import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class I75ScraperStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Creates the S3 bucket
     const bucket = new Bucket(this, 'ScoresBucket', {
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // for dev
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
 
     // Creates the Lambda function
-    const fn = new Function(this, 'ScraperFn', {
+    const fn = new NodejsFunction(this, 'ScraperFn', {
       runtime: Runtime.NODEJS_20_X,
-      code: Code.fromAsset('../dist'),
-      handler: 'handler.handler',
+      entry: path.join(__dirname, '../../src/handler.ts'),
+      handler: 'handler',
+      bundling: {
+        // Keeps the Lambda runtime’s AWS SDK external, but also minify & source-map
+        externalModules: ['aws-sdk'],
+        minify: true,
+        sourceMap: true,
+      },
       environment: {
-        SCORES_BUCKET: bucket.bucketName
+        SCORES_BUCKET: bucket.bucketName,
       },
     });
 
     // Grant write permissions
     bucket.grantPut(fn);
 
-    // Schedules it every 5 minutes
+    // EventBridge rule that schedules it every 5 minutes
     new Rule(this, 'ScrapeSchedule', {
       schedule: Schedule.rate(cdk.Duration.minutes(5)),
       targets: [new LambdaFunction(fn)],
